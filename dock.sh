5f787565884b8b4206142e48df4c95a2400f625c
Brad <brdthmpson@gmail.com>
2024-09-15 12:54:27 -0700

    Update dock.sh

--- dock.sh
+++ dock.sh
@@ -6,7 +6,7 @@ dockutil --no-restart --add "/Applications/Ableton Live 10 Suite.app"
 dockutil --no-restart --add "/Applications/Google Chrome Beta.app"
 dockutil --no-restart --add "/System/Applications/Music.app"
 dockutil --no-restart --add "/Applications/Telegram.app"
-docktuil --no-restart --add "/Applications/Discord.app"
+dockutil --no-restart --add "/Applications/Discord.app"
 dockutil --no-restart --add "/Applications/1Password.app"
 
 killall Dock
