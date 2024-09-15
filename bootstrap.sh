651fccca074fe2892d63490cc299437a9708df13
Brad Thompson <brdthmpson@gmail.com>
2024-09-14 19:53:52 -0700

    nichijou time. add macsetup stuff to bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,34 @@ LOG_FILE='/Users/'$(logname)'/Desktop/install-log.log'
 exec &> >(tee -a "$LOG_FILE")
 echo "Logging to" $LOG_FILE
 
+# EDIT THIS FIRST!
+COMPUTER_NAME=yuuko # selamat pagi
+
+# Force password entry after sleep
+defaults write com.apple.screensaver askForPassword 1 
+
+echo "Enabling the firewall..."
+defaults write /Library/Preferences/com.apple.alf globalstate -int 1
+
+# Enable FilevVault
+if [ "$(fdesetup status)" == "FileVault is On." ]; then
+  echo "Disk encryption is enabled. 🔥"
+fi
+
+if [ "$(fdesetup status)" == "FileVault is Off." ]; then
+  echo "Disk encryption not enabled. Enabling now..."
+  fdesetup enable
+fi
+
+# Set computer name
+echo "Changing computer name to: $COMPUTER_NAME"
+sudo scutil --set ComputerName "$COMPUTER_NAME"
+sudo scutil --set HostName "$COMPUTER_NAME"
+sudo scutil --set LocalHostName "$COMPUTER_NAME"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPUTER_NAME"
+
+echo "Computer renamed to: $COMPUTER_NAME"
+
 # run_chezmoi bootstraps chezmoi and runs it with our dotfiles
 run_chezmoi() {
   log " -> Running chezmoi"
