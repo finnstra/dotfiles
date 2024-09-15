d6773bca7dd6aa611286af18010d885819938e57
Brad Thompson <brdthmpson@gmail.com>
2024-09-15 01:34:45 -0700

    update boostrap script after inital testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-# Script to setup a new machine.
+# Script to setup a new machine. Do not run this as sudo, other
+# scripts will not like this and refuse to run. Instead, run
+# this script normally and allow the password prompts to
+# run priviliged commands. 
 
 # Set up logging environment
 LOG_FILE='/Users/'$(logname)'/Desktop/install-log.log'
@@ -11,10 +14,10 @@ echo "Logging to" $LOG_FILE
 COMPUTER_NAME=yuuko # selamat pagi
 
 # Force password entry after sleep
-defaults write com.apple.screensaver askForPassword 1 
+sudo defaults write com.apple.screensaver askForPassword 1 
 
 echo "Enabling the firewall..."
-defaults write /Library/Preferences/com.apple.alf globalstate -int 1
+sudo defaults write /Library/Preferences/com.apple.alf globalstate -int 1
 
 # Enable FilevVault
 if [ "$(fdesetup status)" == "FileVault is On." ]; then
@@ -33,11 +36,15 @@ sudo scutil --set HostName "$COMPUTER_NAME"
 sudo scutil --set LocalHostName "$COMPUTER_NAME"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPUTER_NAME"
 
-echo "Computer renamed to: $COMPUTER_NAME"
+echo "Computer renamed to: $COMPUTER_NAME ✅"
+
+# Install oh-my-sh first, otherwise the terminal will look weird
+echo "-> installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # run_chezmoi bootstraps chezmoi and runs it with our dotfiles
 run_chezmoi() {
-  log " -> Running chezmoi"
+  echo " -> Running chezmoi"
   if ! command -v chezmoi >/dev/null; then
     sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply finnstra
   else
@@ -45,23 +52,23 @@ run_chezmoi() {
   fi
 }
 
-run_chezmoi
-
-# After chezmoi syncs everything, we can have Homebrew install everything
+# Install homebrew
 install_homebrew() {    
-    log " -> Installing homebrew"
-    if ! command -v brew >/dev/null; then
-      sh -c /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    else  
-      log "ERROR: Unable to install Homebrew...for some reason."
-    fi
+    echo " -> Installing homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
 install_homebrew
 
-use_homebrew() {
-    log " -> Running homebrew"
-    brew bundle --force cleanup # Sync to the Brewfile.
+# Install Homebrew Path 
+install_homebrew_path() {
+  if [  -f /Users/$(logname)/.zprofile ]; then
+    echo "Skipping path creation because .zprofile existed. Path is likely already created."
+  else
+    echo "zprofile not found. creating it to make brew work"
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/brad/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
 }
 
-use_homebrew
+install_homebrew_path 
