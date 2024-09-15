9e3efd7b170b53d9ba455dc22271fb6aa34c55df
Brad Thompson <brdthmpson@gmail.com>
2024-09-15 12:53:11 -0700

    add dock configuration script

---
+++ dock.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+dockutil --no-restart --remove all
+
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/Ableton Live 10 Suite.app"
+dockutil --no-restart --add "/Applications/Google Chrome Beta.app"
+dockutil --no-restart --add "/System/Applications/Music.app"
+dockutil --no-restart --add "/Applications/Telegram.app"
+docktuil --no-restart --add "/Applications/Discord.app"
+dockutil --no-restart --add "/Applications/1Password.app"
+
+killall Dock
+
+echo "dock configured."
