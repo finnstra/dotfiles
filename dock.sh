42908d78d23051e338c550cce615f142d039ab1d
Brad Thompson <3990025+finnstra@users.noreply.github.com>
2024-09-16 09:23:47 -0700

    Add rekordbox to the dock

--- dock.sh
+++ dock.sh
@@ -6,6 +6,7 @@ dockutil --no-restart --add "/Applications/Ableton Live 10 Suite.app"
 dockutil --no-restart --add "/Applications/Google Chrome Beta.app"
 dockutil --no-restart --add "/System/Applications/Music.app"
 dockutil --no-restart --add "/Applications/Telegram.app"
+dockutil --no-restart --add "/Applications/rekordbox 6/rekordbox.app"
 dockutil --no-restart --add "/Applications/Discord.app"
 dockutil --no-restart --add "/Applications/1Password.app"
 
