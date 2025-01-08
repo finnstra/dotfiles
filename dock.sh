395884ff8d817fc797214afcec5ccab0a2e9d443
Brad Thompson <3990025+finnstra@users.noreply.github.com>
2025-01-07 21:50:04 -0800

    Update dock, I use Ableton 12 now.

--- dock.sh
+++ dock.sh
@@ -2,14 +2,13 @@
 dockutil --no-restart --remove all
 
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-dockutil --no-restart --add "/Applications/Ableton Live 10 Suite.app"
+dockutil --no-restart --add "/Applications/Ableton Live 12 Suite.app"
 dockutil --no-restart --add "/Applications/Google Chrome Beta.app"
 dockutil --no-restart --add "/System/Applications/Music.app"
 dockutil --no-restart --add "/Applications/Telegram.app"
 dockutil --no-restart --add "/Applications/rekordbox 6/rekordbox.app"
 dockutil --no-restart --add "/Applications/Discord.app"
 dockutil --no-restart --add "/Applications/1Password.app"
-dockutil --no-restart --add "/Applications/TickTick.app"
 
 killall Dock
 
